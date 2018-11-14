8cc4500e567e1a059ce3391a6621acc3a02625ba
johnhosolirius <john.ho@solirius.com>
2018-11-14 09:25:25 +0000

    Fix missing variable

--- asp.tf
+++ asp.tf
@@ -1,8 +1,9 @@
    locals {
  
       ase_name = "core-compute-${var.env}"
-      common_tags = {
-      }
+
+     // I2 in prod like env, I1 everywhere else
+     sku_size = "${var.env == "prod" || var.env == "sprod" || var.env == "aat" ? "I2" : "I1"}"
     }
  
  
@@ -14,8 +15,8 @@
       env = "${var.env}"
       resource_group_name = "${azurerm_resource_group.rg.name}"
       asp_capacity = "${var.asp_capacity}"
-      asp_sku_size = "${var.asp_sku_size}"  // Specifies the plan's instance size (set to "I1")
+      asp_sku_size = "${local.sku_size}"  // Specifies the plan's instance size (set to "I1")
       asp_name = "${var.product}"
       ase_name = "${local.ase_name}"
-      tag_list = "${local.common_tags}"
+      tag_list = "${local.tags}"
 }
